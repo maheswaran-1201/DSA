cb865ac1113e28256886835bace2bdfa63dd4b7e
maheswaran-1201 <mahesrajan07@gmail.com>
2026-09-01 15:00:04 +0530

    quicksort.py

--- week4/quicksort.py
+++ week4/quicksort.py
@@ -1,23 +1,23 @@
-def quick_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    
-    pivot = arr[len(arr) // 2]
-    left = []
-    middle = []
-    right = []
-    for x in arr:
-        if x < pivot:
-            left.append(x)
-        elif x == pivot:
-            middle.append(x)
-        else:
-            right.append(x)
-    
-    return quick_sort(left) + middle + quick_sort(right)
-
-arr = []
-z = int(input("Enter the number of elements: "))
-for k in range(z):
-    arr.append(int(input("Enter the element: ")))
-print("Sorted array:", quick_sort(arr))
+def quicksort(a, low, high):
+    if low < high:
+        i = low + 1
+        j = high
+        pivot = low
+        while i <= j:
+            while i <= high and a[i] <= a[pivot]:
+                i += 1
+            while j >= low and a[j] > a[pivot]:
+                j -= 1
+            if i < j:
+                a[i], a[j] = a[j], a[i]
+        a[pivot], a[j] = a[j], a[pivot]
+        quicksort(a, low, j - 1)
+        quicksort(a, j + 1, high)
+n = int(input("enter the number of elements"))
+a=[]
+for i in range(n):
+    k=int(input("enter the element"))
+    a.append(k)
+quicksort(a, 0, n - 1)
+print("Sorted array:")
+print(a)
