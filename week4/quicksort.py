9f560b8e292f8138e6af8468d9157bc99e2bcb9b
maheswaran-1201 <mahesrajan07@gmail.com>
2026-09-01 14:53:25 +0530

    Add mergesort and quicksort algorithms

---
+++ week4/quicksort.py
@@ -0,0 +1,23 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    pivot = arr[len(arr) // 2]
+    left = []
+    middle = []
+    right = []
+    for x in arr:
+        if x < pivot:
+            left.append(x)
+        elif x == pivot:
+            middle.append(x)
+        else:
+            right.append(x)
+    
+    return quick_sort(left) + middle + quick_sort(right)
+
+arr = []
+z = int(input("Enter the number of elements: "))
+for k in range(z):
+    arr.append(int(input("Enter the element: ")))
+print("Sorted array:", quick_sort(arr))
